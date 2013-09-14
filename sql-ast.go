537b6d6e8cf933837231866ba89bd5332ededdd1
Aaron Ackerman <theron17@gmail.com>
2013-09-14 15:57:03 -0500

    Saved file to update build

--- sql-ast.go
+++ sql-ast.go
@@ -9,33 +9,3 @@ type SqlAst interface {
 	CreateOn(string) *OnNode
 	CreateGrouping(string) *GroupingNode
 }
-
-type FactoryMethods struct{}
-
-func (f *FactoryMethods) CreateTrue() *TrueNode {
-	return NewTrueNode()
-}
-
-func (f *FactoryMethods) CreateFalse() *FalseNode {
-	return NewFalseNode()
-}
-
-func (f *FactoryMethods) CreateTableAlias(relation *Table, name string) *TableAliasNode {
-	return NewTableAliasNode(relation, name)
-}
-
-func (f *FactoryMethods) CreateJoin(to string) *JoinNode {
-	return &JoinNode{}
-}
-
-func (f *FactoryMethods) CreateAnd(clauses []interface{}) *AndNode {
-	return NewAndNode()
-}
-
-func (f *FactoryMethods) CreateOn(expr string) *OnNode {
-	return NewOnNode()
-}
-
-func (f *FactoryMethods) CreateGrouping(expr string) *GroupingNode {
-	return NewGroupingNode()
-}
