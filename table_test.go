6bf06f510c6dee67550cda84dcdb847c0a0af888
Aaron Ackerman <theron17@gmail.com>
2013-12-22 17:22:59 -0600

    Created test for projections

--- table_test.go
+++ table_test.go
@@ -14,3 +14,22 @@ func TestTable(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestTableName(t *testing.T) {
+	engine := NewEngine()
+	table := NewTable("users", engine)
+	if table.Name != "users" {
+		t.Fail()
+	}
+}
+
+func TestTableProjections(t *testing.T) {
+	engine := NewEngine()
+	table := NewTable("users", engine)
+	query := table.Project(Sql("*"), Sql("*"))
+	sql := query.ToSql()
+	if sql != "SELECT *, * FROM \"users\"" {
+		t.Logf("TestTable sql: %s", sql)
+		t.Fail()
+	}
+}
