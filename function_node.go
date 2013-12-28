8d026b425a6ef906224d8ccee3bfbc9652cb202e
Aaron Ackerman <theron17@gmail.com>
2013-12-27 18:30:59 -0600

    Added several other types of FunctionNode's

--- function_node.go
+++ function_node.go
@@ -3,3 +3,9 @@ package arel
 type FunctionNode struct {
 	BaseNode
 }
+
+type SumNode FunctionNode
+type ExistsNode FunctionNode
+type MaxNode FunctionNode
+type MinNode FunctionNode
+type AvgNode FunctionNode
