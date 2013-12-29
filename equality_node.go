6d10efd0b045f98ddf3760022b783d4a9284928c
Aaron Ackerman <theron17@gmail.com>
2013-12-29 12:23:14 -0600

    Added NewEqualityNode function

--- equality_node.go
+++ equality_node.go
@@ -1,4 +1,16 @@
 package arel
 
-type EqualityNode BinaryNode
+type EqualityNode struct {
+	Right *Attribute
+	Left  *Attribute
+	BaseNode
+}
+
+func NewEqualityNode(right *Attribute, left *Attribute) EqualityNode {
+	return EqualityNode{
+		Right: right,
+		Left:  left,
+	}
+}
+
 type InNode EqualityNode
