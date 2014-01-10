c2c7a9d00af03e5664bdbfcf5affcdc0bd679804
Aaron Ackerman <theron17@gmail.com>
2014-01-09 19:27:48 -0600

    Created not equal test for delete statement node

--- delete_statement_node_test.go
+++ delete_statement_node_test.go
@@ -17,3 +17,17 @@ func TestDeleteStatementNodeEq(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestDeleteStatementNodeNotEq(t *testing.T) {
+	ds1 := DeleteStatementNode{
+		Wheres: &[]Visitable{Sql("a"), Sql("b"), Sql("c")},
+	}
+
+	ds2 := DeleteStatementNode{
+		Wheres: &[]Visitable{Sql("1"), Sql("2"), Sql("3")},
+	}
+
+	if ds1.Eq(ds2) {
+		t.Fail()
+	}
+}
