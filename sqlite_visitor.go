2d1cd05a0211d48555d2c8cdced8f060ece75229
Aaron Ackerman <theron17@gmail.com>
2014-01-12 19:36:37 -0600

    Implemented SQLiteVisitor

---
+++ sqlite_visitor.go
@@ -0,0 +1,32 @@
+package rel
+
+type SQLiteVisitor struct {
+	ToSqlVisitor
+}
+
+func (v SQLiteVisitor) Accept(a Visitable) string {
+	return v.Visit(a)
+}
+
+func (v SQLiteVisitor) Visit(a Visitable) string {
+	switch val := a.(type) {
+	case *LockNode:
+		return v.VisitLockNode(*val)
+	case *SelectStatementNode:
+		return v.VisitSelectStatementNode(*val)
+	default:
+		return v.ToSqlVisitor.Visit(a)
+	}
+}
+
+// Locks are not supported in SQLite
+func (v SQLiteVisitor) VisitLockNode(node LockNode) string {
+	return ""
+}
+
+func (v SQLiteVisitor) VisitSelectStatementNode(node SelectStatementNode) string {
+	if node.Offset != nil && node.Limit == nil {
+		node.Limit = &LimitNode{Expr: Sql("-1")}
+	}
+	return v.ToSqlVisitor.VisitSelectStatementNode(node)
+}
