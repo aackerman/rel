c0fd6617ad449fcd3d43e4b5f43f9bc984a26563
Aaron Ackerman <theron17@gmail.com>
2014-01-14 20:22:39 -0600

    Finished tests for ExtractNode

--- extract_node_test.go
+++ extract_node_test.go
@@ -4,19 +4,22 @@ import (
 	. "."
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	// "testing"
 )
 
 var _ = Describe("ExtractNode", func() {
-	BeforeEach(func() {
-		Register("postgresql", NewEngine())
-	})
-
 	It("should extract field", func() {
 		table := NewTable("users")
 		mgr := table.From(table)
 		mgr.Project(table.Attr("timestamp").Extract(Sql("date")))
 		Expect(mgr.ToSql()).To(Equal("SELECT EXTRACT(DATE FROM \"users\".\"timestamp\") FROM \"users\""))
+	})
+
+	Describe("As", func() {
+		It("should alias the Extract", func() {
+			table := NewTable("users")
+			mgr := table.From(table)
+			mgr.Project(table.Attr("timestamp").Extract(Sql("date")).As(Sql("foo")))
+			Expect(mgr.ToSql()).To(Equal("SELECT EXTRACT(DATE FROM \"users\".\"timestamp\") AS foo FROM \"users\""))
+		})
 	})
 })
