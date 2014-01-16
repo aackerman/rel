7d0f524589f1ea1823703354ca25a47f4973be33
Aaron Ackerman <theron17@gmail.com>
2014-01-15 18:31:36 -0600

    Converted AndNode tests to Ginkgo

--- and_node_test.go
+++ and_node_test.go
@@ -2,13 +2,14 @@ package rel_test
 
 import (
 	. "."
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func TestAndNodeEq(t *testing.T) {
-	and1 := AndNode{Children: &[]Visitable{Sql("foo"), Sql("bar")}}
-	and2 := AndNode{Children: &[]Visitable{Sql("foo"), Sql("bar")}}
-	if !and1.Eq(and2) {
-		t.Fail()
-	}
-}
+var _ = Describe("AndNode", func() {
+	It("can be equal to other AndNode's", func() {
+		and1 := AndNode{Children: &[]Visitable{Sql("foo"), Sql("bar")}}
+		and2 := AndNode{Children: &[]Visitable{Sql("foo"), Sql("bar")}}
+		Expect(and1).To(Equal(and2))
+	})
+})
