71d225c748c92a4f341e79efd18fcf854166a065
Aaron Ackerman <theron17@gmail.com>
2014-01-15 18:45:25 -0600

    Implemented CountNode tests in Ginkgo

--- count_node_test.go
+++ count_node_test.go
@@ -2,24 +2,22 @@ package rel_test
 
 import (
 	. "."
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func TestCountNodeAs(t *testing.T) {
-	table := NewTable("users")
-	mgr := table.Select(table.Attr("id").Count().As(Sql("foo")))
-	sql := mgr.ToSql()
-	expected := "SELECT COUNT(\"users\".\"id\") AS foo FROM \"users\""
-	if sql != expected {
-		t.Logf("TestCoundNodeAs sql: \n%s != \n%s", sql, expected)
-		t.Fail()
-	}
-}
+var _ = Describe("CountNode", func() {
+	It("can be equal to other CountNode's", func() {
+		count1 := CountNode{Expressions: []Visitable{Sql("foo")}}
+		count2 := CountNode{Expressions: []Visitable{Sql("foo")}}
+		Expect(count1.Eq(count2)).To(BeTrue())
+	})
 
-func TestCountNodeEq(t *testing.T) {
-	count1 := CountNode{Expressions: []Visitable{Sql("foo")}}
-	count2 := CountNode{Expressions: []Visitable{Sql("foo")}}
-	if !count1.Eq(count2) {
-		t.Fail()
-	}
-}
+	It("can use the As predication to be aliased", func() {
+		table := NewTable("users")
+		mgr := table.Select(table.Attr("id").Count().As(Sql("foo")))
+		sql := mgr.ToSql()
+		expected := "SELECT COUNT(\"users\".\"id\") AS foo FROM \"users\""
+		Expect(sql).To(Equal(expected))
+	})
+})
