c5b4eb09c125e189727c4904beb8353468650b72
Aaron Ackerman <theron17@gmail.com>
2014-01-16 19:39:02 -0600

    Added predications to GroupingNodes

--- grouping_node.go
+++ grouping_node.go
@@ -4,3 +4,151 @@ type GroupingNode struct {
 	Expr []Visitable
 	BaseVisitable
 }
+
+func (node GroupingNode) Eq(visitable Visitable) *EqualityNode {
+	return predicationEq(node, visitable)
+}
+
+func (node GroupingNode) EqAny(visitables ...Visitable) *GroupingNode {
+	return predicationEqAny(node, visitables...)
+}
+
+func (node GroupingNode) EqAll(visitables ...Visitable) *GroupingNode {
+	return predicationEqAll(node, visitables...)
+}
+
+func (node GroupingNode) Lt(visitable Visitable) *LessThanNode {
+	return predicationLt(node, visitable)
+}
+
+func (node GroupingNode) LtAny(visitables ...Visitable) *GroupingNode {
+	return predicationLtAny(node, visitables...)
+}
+
+func (node GroupingNode) LtAll(visitables ...Visitable) *GroupingNode {
+	return predicationLtAll(node, visitables...)
+}
+
+func (node GroupingNode) LtEq(v Visitable) *LessThanOrEqualNode {
+	return &LessThanOrEqualNode{Left: node, Right: v}
+}
+
+func (node GroupingNode) LtEqAny(visitables ...Visitable) *GroupingNode {
+	return predicationLtEqAny(node, visitables...)
+}
+
+func (node GroupingNode) LtEqAll(visitables ...Visitable) *GroupingNode {
+	return predicationLtEqAll(node, visitables...)
+}
+
+func (node GroupingNode) Gt(visitable Visitable) *GreaterThanNode {
+	return predicationGt(node, visitable)
+}
+
+func (node GroupingNode) GtAny(visitables ...Visitable) *GroupingNode {
+	return predicationGtAny(node, visitables...)
+}
+
+func (node GroupingNode) GtAll(visitables ...Visitable) *GroupingNode {
+	return predicationGtAll(node, visitables...)
+}
+
+func (node GroupingNode) GtEq(visitable Visitable) *GreaterThanOrEqualNode {
+	return predicationGtEq(node, visitable)
+}
+
+func (node GroupingNode) GtEqAny(visitables ...Visitable) *GroupingNode {
+	return predicationGtEqAny(node, visitables...)
+}
+
+func (node GroupingNode) GtEqAll(visitables ...Visitable) *GroupingNode {
+	return predicationGtEqAll(node, visitables...)
+}
+
+func (node GroupingNode) Desc() *DescendingNode {
+	return predicationDesc(node)
+}
+
+func (node GroupingNode) Asc() *AscendingNode {
+	return predicationAsc(node)
+}
+
+func (node GroupingNode) Count() *CountNode {
+	return predicationCount(node)
+}
+
+func (node GroupingNode) Extract(literal SqlLiteralNode) *ExtractNode {
+	return predicationExtract(node, literal)
+}
+
+func (node GroupingNode) As(visitable Visitable) *AsNode {
+	return predicationAs(node, visitable)
+}
+
+func (node GroupingNode) In(visitables []Visitable) Visitable {
+	return predicationIn(node, visitables)
+}
+
+func (node GroupingNode) InAny(visitableslices ...[]Visitable) Visitable {
+	return predicationInAny(node, visitableslices...)
+}
+
+func (node GroupingNode) InAll(visitableslices ...[]Visitable) Visitable {
+	return predicationInAll(node, visitableslices...)
+}
+
+func (node GroupingNode) NotIn(visitables []Visitable) Visitable {
+	return predicationNotIn(node, visitables)
+}
+
+func (node GroupingNode) NotInAny(visitableslices ...[]Visitable) Visitable {
+	return predicationNotInAny(node, visitableslices...)
+}
+
+func (node GroupingNode) NotInAll(visitableslices ...[]Visitable) Visitable {
+	return predicationNotInAll(node, visitableslices...)
+}
+
+func (node GroupingNode) NotEq(visitable Visitable) *NotEqualNode {
+	return predicationNotEq(node, visitable)
+}
+
+func (node GroupingNode) NotEqAny(visitables ...Visitable) *GroupingNode {
+	return predicationNotEqAny(node, visitables...)
+}
+
+func (node GroupingNode) NotEqAll(visitables ...Visitable) *GroupingNode {
+	return predicationNotEqAll(node, visitables...)
+}
+
+func (node GroupingNode) DoesNotMatch(literal SqlLiteralNode) *DoesNotMatchNode {
+	return predicationDoesNotMatch(node, literal)
+}
+
+func (node GroupingNode) DoesNotMatchAny(literals ...SqlLiteralNode) *GroupingNode {
+	return predicationDoesNotMatchAny(node, literals...)
+}
+
+func (node GroupingNode) DoesNotMatchAll(literals ...SqlLiteralNode) *GroupingNode {
+	return predicationDoesNotMatchAll(node, literals...)
+}
+
+func (node GroupingNode) Matches(literal SqlLiteralNode) *MatchesNode {
+	return predicationMatches(node, literal)
+}
+
+func (node GroupingNode) MatchesAny(literals ...SqlLiteralNode) *GroupingNode {
+	return predicationMatchesAny(node, literals...)
+}
+
+func (node GroupingNode) MatchesAll(literals ...SqlLiteralNode) *GroupingNode {
+	return predicationMatchesAll(node, literals...)
+}
+
+func (node GroupingNode) GroupAny(visitables ...Visitable) *GroupingNode {
+	return predicationGroupAny(node, visitables...)
+}
+
+func (node GroupingNode) GroupAll(visitables ...Visitable) *GroupingNode {
+	return predicationGroupAll(node, visitables...)
+}
