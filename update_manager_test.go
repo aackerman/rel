307fe3320f1613d886ab882e1415802134191a2b
Aaron Ackerman <theron17@gmail.com>
2014-02-02 11:17:28 -0600

    Added a few more tests for UpdateManager

--- update_manager_test.go
+++ update_manager_test.go
@@ -24,4 +24,30 @@ var _ = Describe("UpdateManager", func() {
 		mgr.Set(table.Attr("name"), nil)
 		Expect(mgr.ToSql()).To(Equal("UPDATE \"users\" SET \"name\" = NULL WHERE \"users\".\"id\" IN (SELECT \"users\".\"id\" FROM \"users\" LIMIT 10)"))
 	})
+
+	It("updates with null", func() {
+		table := NewTable("users")
+		mgr := NewUpdateManager(RelEngine)
+		mgr.SetTable(table)
+		mgr.Set(table.Attr("name"), nil)
+		Expect(mgr.ToSql()).To(Equal("UPDATE \"users\" SET \"name\" = NULL"))
+	})
+
+	Describe("SetTable", func() {
+		It("sets the relation", func() {
+			mgr := NewUpdateManager(RelEngine)
+			mgr.SetTable(NewTable("users"))
+			Expect(mgr.ToSql()).To(Equal("UPDATE \"users\""))
+		})
+	})
+
+	Describe("Where", func() {
+		It("generates a where clause", func() {
+			table := NewTable("users")
+			mgr := NewUpdateManager(RelEngine)
+			mgr.SetTable(table)
+			mgr.Where(table.Attr("id").Eq(Sql(1)))
+			Expect(mgr.ToSql()).To(Equal("UPDATE \"users\" WHERE \"users\".\"id\" = 1"))
+		})
+	})
 })
