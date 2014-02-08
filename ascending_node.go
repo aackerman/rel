cc30bf92c079553cbf9fefea70820d5e4f8aa5ee
Aaron Ackerman <theron17@gmail.com>
2014-02-08 17:03:19 -0600

    Use ptrs to ascending nodes

--- ascending_node.go
+++ ascending_node.go
@@ -10,10 +10,10 @@ func (node AscendingNode) Eq(other AscendingNode) bool {
 	return reflect.DeepEqual(node, other)
 }
 
-func (node AscendingNode) Direction() string {
+func (node *AscendingNode) Direction() string {
 	return "ASC"
 }
 
-func (node AscendingNode) Reverse() *DescendingNode {
+func (node *AscendingNode) Reverse() *DescendingNode {
 	return &DescendingNode{Expr: node.Expr}
 }
