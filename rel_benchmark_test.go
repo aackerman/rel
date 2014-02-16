ad8301dbfbd576e043b7105d571e1f54608ce5dd
Aaron Ackerman <theron17@gmail.com>
2014-02-16 12:13:53 -0600

    Created Join benchmark

--- rel_benchmark_test.go
+++ rel_benchmark_test.go
@@ -10,3 +10,11 @@ func BenchmarkSelect(b *testing.B) {
 		Select().ToSql()
 	}
 }
+
+func BenchmarkSelectJoin(b *testing.B) {
+	users := NewTable("users")
+	preferences := NewTable("preferences")
+	for i := 0; i < b.N; i++ {
+		Select(Star()).Join(preferences).On(preferences.Attr("user_id").Eq(users.Attr("user_id"))).ToSql()
+	}
+}
